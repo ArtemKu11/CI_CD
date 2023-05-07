2888cc675084ee1fe4e1921d8ae9c36ab6cfcefb
artem_kuchumov <kuchumov.a.m@gmail.com>
2023-05-07 21:59:41 +0500

    Кнопочка Загрузить fix

--- spring_app_frontend/src/components/tableWindow/NoneFilesDiv.vue
+++ spring_app_frontend/src/components/tableWindow/NoneFilesDiv.vue
@@ -2,7 +2,8 @@
     <div class="none-files-div">
         <span>Файлов не найдено</span>
         <DefaultButton v-if="filters" @click="filtersClick" :buttonText="'Применить фильтры/загрузить'" />
-        <DefaultButton v-else :buttonText="'Загрузить'" />
+        <DefaultButton @click="activateForm" v-else :buttonText="'Загрузить'" />
+        <input ref="input" @change="fileFormChange" id="fileform" type="file">
     </div>
 </template>
 
@@ -10,6 +11,9 @@
 import { defineComponent } from 'vue';
 import DefaultButton from '@/components/Button.vue'
 import { FilesFilters } from '@/store/table/types';
+import { httpClient } from '@/api/AxiosHttpClient';
+import { Alerts } from '@/store/alerts/helpers';
+import { InfoAlertType } from '@/store/alerts/types';
 
 
 export default defineComponent({
@@ -20,13 +24,49 @@ export default defineComponent({
     methods: {
         filtersClick() {
             this.$store.commit('windowFlags/setFiltersFlag', true)
-        }
+        },
+
+        activateForm() {
+            const input = this.$refs.input as HTMLInputElement
+            if (input) {
+                input.click()
+            }
+        }, 
+
+        async fileFormChange(e: Event) {
+            const input = e.target as HTMLFormElement
+            const filesArr = input.files
+            if (filesArr && filesArr.length) {
+                const formData = new FormData
+                formData.append('file', filesArr[0])
+                const response = await httpClient.post('files/new/formdata', formData)
+                if (response && response.status && response.status === 200) {
+                    const alert: InfoAlertType = {
+                        message: 'Загружено успешно',
+                        time: 1500,
+                        type: 'green'
+                    }
+                    Alerts.showInfoAlert(alert)
+                    this.refresh()
+                }
+            }
+        },
+
+        refresh() {
+            this.$store.commit('windowFlags/setFiltersFlag', false)
+            if (this.filters) {
+                this.$store.dispatch('table/loadFiles', this.filters)
+            } else {
+                this.$store.dispatch('table/loadFiles')
+            }
+        },
     },
     computed: {
         filters(): FilesFilters {
             return this.$store.getters['filters/getFilters'] as FilesFilters
-        }
-    }
+        },
+    },
+
 });
 </script>
 
@@ -45,6 +85,10 @@ export default defineComponent({
         button {
             font-size: $smallFontSize;
         }
+    }
+
+    input {
+        display: none;
     }
 }
 </style>
